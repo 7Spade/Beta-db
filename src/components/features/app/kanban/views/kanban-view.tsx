e1ba991394898f288e9b2be8f4b362f253e99acd
ac (𓂸) <ac7x7x@gmail.com>
2025-08-27 18:02:35 +0000

    1

--- src/components/features/app/kanban/views/kanban-view.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { KanbanBoard } from "../components/kanban-board";
-import { columns, tasks } from "../data";
-
-export const KanbanView = () => {
-    return <KanbanBoard columns={columns} tasks={tasks} />
-}
