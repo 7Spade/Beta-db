89f4af3662494c73fc0ac5d54933a26cd41cbe10
ac (𓂸) <ac7x7x@gmail.com>
2025-08-31 14:52:11 +0000

    1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
-import type { Metadata } from 'next';
-import './globals.css';
-import { Toaster } from '@/ui/toaster';
-import { cn } from '@/utils/utils';
-import { Inter } from 'next/font/google';
 import { AuthProvider } from '@/auth/auth-provider';
+import { Toaster } from '@/ui/toaster';
+import { cn } from '@/utils';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
 const inter = Inter({
   subsets: ['latin'],
