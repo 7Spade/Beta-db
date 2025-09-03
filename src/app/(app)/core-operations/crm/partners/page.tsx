ae3cdb73a321ae4e656958f9eb01e60fea137105
7s.i <7s.i@pm.me>
2025-09-03 10:55:41 +0800

    1

---
+++ src/app/(app)/core-operations/crm/partners/page.tsx
@@ -0,0 +1,5 @@
+import { PartnersView } from '@/features/core-operations/crm/partners/partners-view';
+
+export default function PartnersPage() {
+  return <PartnersView />;
+}
---
+++ src/app/(app)/core-operations/crm/workflows/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { WorkflowBuilder } from '@/features/core-operations/crm/workflows/workflow-builder';
+
+export default function WorkflowsPage() {
+  return <WorkflowBuilder />;
+}
